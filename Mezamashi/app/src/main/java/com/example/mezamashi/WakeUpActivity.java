14f22c7d52a504f5be66294d6d933d6401728712
MingZhe Yang <mei28aquarius@gmail.com>
2020-05-23 20:01:27 +0900

    add shake function

--- Mezamashi/app/src/main/java/com/example/mezamashi/WakeUpActivity.java
+++ Mezamashi/app/src/main/java/com/example/mezamashi/WakeUpActivity.java
@@ -1,6 +1,11 @@
 package com.example.mezamashi;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import com.example.mezamashi.R;
@@ -11,15 +16,27 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
 // 参考 https://github.com/hiroaki-dev/AlarmSample/blob/master/app/src/main/java/me/hiroaki/alarmsample/PlaySoundActivity.java
 
-public class WakeUpActivity extends AppCompatActivity {
+public class WakeUpActivity extends AppCompatActivity implements SensorEventListener {
 
     Button stopBtn;
+    boolean shaked = false;
+
+    private static final float SHAKE_THRESHOLD_GRAVITY = 2.7F;
+    private static final int SHAKE_SLOP_TIME_MS = 500;
+    private static final int SHAKE_COUNT_RESET_TIME_MS = 3000;
+    private long mShakeTimestamp;
+    private int mShakeCount;
+    private int SHAKE_MAX_COUNT = 30;
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +61,53 @@ public class WakeUpActivity extends AppCompatActivity {
             }
         });
 
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor accelerationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, accelerationSensor, SensorManager.SENSOR_DELAY_UI);
+        if (shaked == false) {
+            stopBtn.setEnabled(false);
+        }
 
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+
+        float gX = x / SensorManager.GRAVITY_EARTH;
+        float gY = y / SensorManager.GRAVITY_EARTH;
+        float gZ = z / SensorManager.GRAVITY_EARTH;
+
+        // gForce will be close to 1 when there is no movement.
+        float gForce = (float) Math.sqrt(gX * gX + gY * gY + gZ * gZ);
+
+        if (gForce > SHAKE_THRESHOLD_GRAVITY) {
+            final long now = System.currentTimeMillis();
+            // ignore shake events too close to each other (500ms)
+            if (mShakeTimestamp + SHAKE_SLOP_TIME_MS > now) {
+                return;
+            }
+
+            // reset the shake count after 3 seconds of no shakes
+            if (mShakeTimestamp + SHAKE_COUNT_RESET_TIME_MS < now) {
+                mShakeCount = 0;
+            }
+
+            mShakeTimestamp = now;
+            mShakeCount++;
+        }
+
+//        Log.d("mezamashi", String.valueOf(mShakeCount));
+        if (mShakeCount > SHAKE_MAX_COUNT && shaked == false) {
+            shaked = true;
+            stopBtn.setEnabled(shaked);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
